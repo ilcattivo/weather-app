2b7f0ccb9fcd3c0f24847c36f04d702875d7abb8
dtrubin <dtrubin@antagosoft.com>
2020-07-03 18:59:21 +0300

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h1>Конвентер валют</h1>
+    <div class="container">
+    <div class="row">
+      <div class="col-5">
+        One of three columns
+      </div>
+      <div class="col-2">
+        <img src="/swap.png" width="31px" height="20px" />
+      </div>
+      <div class="col-5">
+        One of three columns
+      </div>
     </div>
-    <router-view/>
+  </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +30,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
